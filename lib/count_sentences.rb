f9e6b1143df41b88abf164c8dd24dafbcdf3f1b4
jackcornblum <jcornblum@gmail.com>
2021-06-29 15:46:54 -0400

    commit tree 03ffcf02dd987aa9fa70952a201f015559345b72

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    split_array = self.split(/\.|\?|\!/).delete_if {|char| char.size < 2}
+    count = split_array.count
+    count
+    # binding.pry
   end
 end
+
+# binding.pry
+0
